ea2750fbf761fed65841ca9ef6c32bd66eb2d06d
Jesse Emana <jesseemana@gmail.com>
2023-09-21 21:34:53 +0200

    fixed styling

--- src/components/Inputs/ContactForm.tsx
+++ src/components/Inputs/ContactForm.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import Input from './Input'
 import Button from '../Button'
 
-const ContactForm = () => {
+interface FormProps {
+  submitting: boolean
+  onSubmit: () => void
+}
+
+const ContactForm = ({onSubmit, submitting}: FormProps) => {
   const handleSubmit = () => { console.log('Form Submitted') }
 
   return (
-    <form onSubmit={handleSubmit} className='flex flex-col gap-3 w-[630px] border px-[70px] py-10 rounded-md'>
+    <form onSubmit={onSubmit} className='flex flex-col gap-3 w-[630px] border px-[70px] py-10 rounded-md'>
       <p className='text-text-color font-semibold'>
         Questions or need assistance? <br /> 
         Let us know about it!
@@ -25,12 +30,13 @@ const ContactForm = () => {
         id='message'
         rows={10} 
         placeholder='Message'
-        className='py-1 px-3 resize-none bg-transparent border border-gray-200 rounded-md'
+        className='py-1 px-3 resize-none bg-transparent border border-gray-700 rounded-md'
       />
       <div className='grid place-items-center'>
         <Button 
           type='submit' 
           label='submit' 
+          disabled={submitting}
         />
       </div>
     </form>
--- src/components/Inputs/Input.tsx
+++ src/components/Inputs/Input.tsx
@@ -3,19 +3,27 @@ import React from 'react'
 type InputProps = {
   id: string
   type: 'text'
+  label?: string
   error?: string
+  htmlFor?: string
   placeholder: string
 }
 
 const Input = (props: InputProps) => {
   return (
     <>
+      <label 
+        htmlFor={props.htmlFor} 
+        className='text-sm md:text-[15px] capitalize text-gray-200'
+      >
+        {props.label}
+      </label>
       <input
         id={props.id}
         type={props.type}
         placeholder={props.placeholder}
         autoComplete='off'
-        className='py-1 md:py-2 px-3 text-sm text-gray-200 border border-gray-200 rounded-md bg-transparent'
+        className='py-1 md:py-3 px-3 text-sm text-gray-200 border border-gray-700 rounded-md bg-transparent w-[300px] outline-none'
       />
       {props.error && <span className='text-red-600 text-sm'>{props.error}</span>}
     </>
