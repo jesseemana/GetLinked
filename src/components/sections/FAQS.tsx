dae35e09eb7fef768e5f5a03c564b0358dd7766f
Jesse Emana <jesseemana@gmail.com>
2023-09-23 14:37:05 +0200

    final styling for this section

--- src/components/sections/FAQS.tsx
+++ src/components/sections/FAQS.tsx
@@ -1,11 +1,11 @@
 import Questions from './Questions'
-import Container from '../components/Container'
+import Container from '@/components/Container'
 
 const FAQS = () => {
   return (
     <section id='questions' className='border-[0.3px] border-l-0 border-r-0 border-b-0 border-gray-500 p-10'>
       <Container>
-        <div className='flex flex-col lg:flex-row-reverse items-center justify-around'>
+        <div className='flex flex-col-reverse gap-10 lg:flex-row-reverse items-center justify-around'>
           <div className='lg:w-1/2 ml-20 relative'>
             <img 
               src='./casual.png' 
@@ -13,16 +13,17 @@ const FAQS = () => {
               className='py-10'
             />
             <img src='./starpu.png' alt='decorator icons' className='absolute top-[170px]' />
-            <img src='./query.png' alt='decorator icons' className='absolute top-[40px] left-[100px] h-12' />
-            <img src='./query.png' alt='decorator icons' className='absolute bottom-[650px] left-[200px] h-20' />
-            <img src='./query.png' alt='decorator icons' className='absolute top-[40px] left-[320px] h-12' />
+            <img src='./query.png' alt='decorator icons' className='absolute top-[7%] left-[18%] h-12' />
+            <img src='./query.png' alt='decorator icons' className='absolute bottom-[90%] left-[33%] h-20' />
+            <img src='./query.png' alt='decorator icons' className='absolute top-[7%] left-[50%] h-12' />
           </div>
-          <div className='text-gray-300 lg:w-1/2 p-7 relative flex flex-col gap-4'>
-            <img src='./starpu.png' alt='decorator icons' className='absolute top-[40px] right-[140px]' />
-            <h1 className='text-white text-3xl font-bold'>Frequently Asked <br /> 
+          <div className='text-gray-200 lg:w-1/2 p-7 relative flex flex-col gap-4 text-md md:text-xl'>
+            <img src='./starpu.png' alt='decorator icons' className='absolute top-[10%] right-[140px]' />
+            <h1 className='text-white text-3xl font-bold mb-2'>Frequently Asked <br /> 
               <span className='text-text-color'>Questions</span>
             </h1>
-            <p>We got answers to the questions that you might
+            <p className='mb-10'>
+              We got answers to the questions that you might
               want to ask about getlinked Hackathon 1.0
             </p>
             <Questions />
--- src/components/sections/Questions.tsx
+++ src/components/sections/Questions.tsx
@@ -9,7 +9,7 @@ const Questions  = () => {
   return (
     <Accordion type="single" collapsible>
       <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionTrigger>Can I work on a project I started before the hackathon?</AccordionTrigger>
         <AccordionContent>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
         Nam rem voluptatem delectus, 
@@ -17,7 +17,7 @@ const Questions  = () => {
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-2">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionTrigger>What happens if I need help during the hackathon?</AccordionTrigger>
         <AccordionContent>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
         Nam rem voluptatem delectus, 
@@ -25,7 +25,7 @@ const Questions  = () => {
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionTrigger>What happens if I don't have an idea for a project?</AccordionTrigger>
         <AccordionContent>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
         Nam rem voluptatem delectus, 
@@ -33,7 +33,7 @@ const Questions  = () => {
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-4">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionTrigger>Can I join a team or do I have to come with one?</AccordionTrigger>
         <AccordionContent>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
         Nam rem voluptatem delectus, 
@@ -41,7 +41,7 @@ const Questions  = () => {
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-5">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionTrigger>What happens after the hackathon ends?</AccordionTrigger>
         <AccordionContent>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
         Nam rem voluptatem delectus, 
@@ -49,7 +49,7 @@ const Questions  = () => {
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-6">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionTrigger>Can I work on a project I started before the hackathon?</AccordionTrigger>
         <AccordionContent>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
         Nam rem voluptatem delectus, 
